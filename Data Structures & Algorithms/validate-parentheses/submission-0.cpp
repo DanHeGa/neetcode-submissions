71b5c92754636171f9087f20004052ab76ef16fb
Daniela Herrera García <A00838702@tec.mx>
2026-05-03 18:28:43 -0600

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stck;
+        bool flag = true;
+
+        unordered_map<char, char> pairs = {
+            {')' , '('},
+            {'}' , '{'}, 
+            {']' , '['}
+        };
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (pairs.count(s[i])) {
+                if (!stck.empty() && stck.top() == pairs[s[i]]) {
+                    stck.pop();
+                } else {
+                    return false; //found it, but not his pair
+                }
+            } else {
+                stck.push(s[i]);
+            }
+        }
+
+        return stck.empty();
+    }
+};
