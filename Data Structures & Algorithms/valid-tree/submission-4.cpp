cdb1c39d5d9d12c7738b8833935a0a74e44ebe19
Daniela Herrera García <A00838702@tec.mx>
2026-06-30 21:56:16 -0600

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) {
+            return false;
+        }
+        
+        vector<vector<int>> adjList(n);
+        for (auto& edge : edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            adjList[node1].push_back(node2);
+            adjList[node2].push_back(node1);
+        }
+
+
+        unordered_set<int> visited;
+        queue<pair<int, int>> q;
+        q.push({0, -1});
+
+        while(!q.empty()) {
+            auto [node, parent] = q.front();
+            q.pop();
+
+            if (visited.find(node) != visited.end()) {
+                return false;
+            }
+
+            visited.insert(node);
+
+            for (int adj : adjList[node]) {
+                if (adj == parent) continue;
+                q.push({adj, node});
+            }
+
+        }
+
+        return visited.size() == n;
+    }
+};
