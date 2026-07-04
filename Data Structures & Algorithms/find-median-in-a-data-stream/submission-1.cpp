2a4217a6078bf7701287d5ffbe85767838d4a9db
Daniela Herrera García <A00838702@tec.mx>
2026-07-03 20:14:37 -0600

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,49 @@
+class MedianFinder {
+private:
+    int size;
+    priority_queue<double, vector<double>, greater<double>> minheap;
+    priority_queue<double> maxheap;
+public:
+    MedianFinder() {
+        this->size = 0;
+    }
+    
+    void addNum(int num) {
+        if (minheap.empty() && maxheap.empty()){
+            maxheap.push(num);
+            size++;
+            return;
+        }
+
+        if (num > maxheap.top()) {
+            minheap.push(num);
+        } else {
+            maxheap.push(num);
+        }
+        
+        //check both heaps stay with a valid size for approach to keep working
+        if (minheap.size() > maxheap.size() + 1) {
+            double ele = minheap.top();
+            minheap.pop();
+            maxheap.push(ele);
+        } else if (maxheap.size() > minheap.size() + 1) { //
+            double ele = maxheap.top();
+            maxheap.pop();
+            minheap.push(ele);
+        }
+
+        size++;
+    }
+    
+    double findMedian() {        
+        if (minheap.size() == maxheap.size() || size % 2 == 0) { 
+            return (maxheap.top() + minheap.top()) / 2;
+        } else { //odd case
+            if (maxheap.size() > minheap.size()) {
+                return maxheap.top();
+            } else {
+                return minheap.top();
+            }
+        }
+    }
+};
