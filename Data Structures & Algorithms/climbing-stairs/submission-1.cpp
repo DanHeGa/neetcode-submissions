90b5e2147df9b07f34fee2f010e25a63f827d33f
Daniela Herrera García <A00838702@tec.mx>
2026-07-04 00:07:22 -0600

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int res;
+
+    int dfs(int n, vector<int>& dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = dfs(n - 1, dp) + dfs(n - 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1,-1);
+        return dfs(n, dp);
+    }
+};
