5f56d91eb998b2cc8b7972a0e42e2624fe5d9065
Daniela Herrera García <A00838702@tec.mx>
2026-07-05 17:30:41 -0600

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+
+        res.push_back(intervals[0]);
+
+        int n = intervals.size();
+        for(int i = 1; i < n; i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[0] = min(res.back()[0], intervals[i][0]);
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;
+    }
+};
