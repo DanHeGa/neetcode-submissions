e5f879d66008cbefad6002c341645ffd90f1ea80
Daniela Herrera García <A00838702@tec.mx>
2026-07-08 23:32:48 -0600

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int, int>>> adjList(n);
+        for(auto &time : times) {
+            pair<int, int> neighbor = {time[2], time[1]}; //(cost, destiny)
+            adjList[time[0]].push_back(neighbor);
+        }
+
+        vector<int> dist(n + 1, INT_MAX); 
+        dist[k] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minheap;
+        pair<int, int> startNodePair = {0, k};
+        minheap.push(startNodePair);
+
+        while(!minheap.empty()) {
+            pair<int, int> currNodePair = minheap.top();
+            int currNode = currNodePair.second;
+
+            minheap.pop();
+
+            for(auto &neigh : adjList[currNode]) {
+                int neighCurrCost = neigh.first;
+                int neighNode = neigh.second;
+
+                if (dist[neighNode] > (dist[currNode] + neighCurrCost)) {
+                    dist[neighNode] = dist[currNode] + neighCurrCost;
+                    minheap.push(neigh);
+                }
+            }
+        }
+
+        int minTime = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] != INT_MAX) {
+                minTime = max(minTime, dist[i]);
+            } else {
+                return -1;
+            }
+        }
+
+        return minTime;
+
+    }
+};
